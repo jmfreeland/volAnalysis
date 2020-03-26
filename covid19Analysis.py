9316912bb2468d774b74ac8b0b73a21ceffdeb29
jmfreeland <josh.freeland@gmail.com>
2020-03-26 12:08:25 +0000

    Update covid19Analysis.py

--- covid19Analysis.py
+++ covid19Analysis.py
@@ -27,21 +27,27 @@ def sir_model(y, x, beta, gamma):
 
 #fit a globla model, so far doesn't do wel
 #import data from hopkins set
-confirmed = pd.read_csv('../COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv')
+confirmed = pd.read_csv('../COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
 #confirmed_global = pd.read_csv('../COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
-recovered = pd.read_csv('../COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv')
-deaths = pd.read_csv('../COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv')
+recovered = pd.read_csv('../COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv')
+deaths = pd.read_csv('../COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv')
 #fit a global model
 
 global_population = 7794798739
+
+
 
 confirmed_cases_global = confirmed.sum()
 confirmed_cases_global = confirmed_cases_global[5:]
 recovered_cases_global = recovered.sum()
 recovered_cases_global = recovered_cases_global[5:]
+if(len(recovered_cases_global)<len(confirmed_cases_global)):
+    last_rec_day = len(recovered_cases_global)
+    recovered_cases_global.loc[confirmed_cases_global.index[-1]] = recovered_cases_global.iloc[last_rec_day-1]*recovered_cases_global.iloc[last_rec_day-1]/recovered_cases_global.iloc[last_rec_day-2]
 deaths_global = deaths.sum()
 deaths_global = deaths_global[5:]
-active_cases_global = confirmed_cases_global - deaths_global - recovered_cases_global
+active_cases_global = pd.Series(confirmed_cases_global.values - deaths_global.values - recovered_cases_global.values, index = confirmed_cases_global.index)
+
 
 days =  range(len(active_cases_global.index))
 
@@ -64,7 +70,7 @@ def temp_odeint(x, beta, gamma):
     return fit_odeint((S0, I0, R0), x, beta, gamma)
 
 #fit the curve using actual data
-popt, pcov = optimize.curve_fit(temp_odeint, xdata, ydata, p0=[5,5])
+popt, pcov = optimize.curve_fit(temp_odeint, xdata, ydata, p0=[3,3])
 print("Global N: " + str(N), ' S0' + str(S0) + ' I0: ' + str(I0) + 'R0: ' + str(R0))
 print(xdata)
 print(ydata) 
@@ -79,7 +85,7 @@ plt.show()
 
 #fit individual country models
 #model_countries = ['Italy','Spain','United Kingdom', 'China', 'US']
-model_countries = ['Italy', 'US', 'Spain', 'China', 'United Kingdom', 'Korea, South']
+model_countries = ['Italy', 'US', 'Spain', 'China', 'United Kingdom', 'Korea, South', 'Germany','Iran', 'Brazil','France','India','Indonesia','Pakistan']
 population = {}
 population['Italy'] = 60480000
 population['Spain'] = 46600000
@@ -87,14 +93,28 @@ population['US'] = 327300000
 population['United Kingdom'] = 66400000
 population['China'] = 1386000000
 population['Korea, South'] = 51470000
+population['Germany'] = 82790000
+population['Iran'] = 81160000
+population['Brazil'] = 209300000
+population['France'] = 66900000
+population['India'] = 1339000000
+population['Indonesia'] = 273000000
+population['Pakistan'] = 220000000
 
 start_day = {}
 start_day['Italy'] = 31
-start_day['US'] = 32
-start_day['Spain'] = 30
+start_day['US'] = 33
+start_day['Spain'] = 32
 start_day['China'] = 6
-start_day['United Kingdom'] = 30
-start_day['Korea, South'] = 10
+start_day['United Kingdom'] = 34
+start_day['Korea, South'] = 26
+start_day['Germany'] = 33
+start_day['Iran'] = 30
+start_day['Brazil'] = 30
+start_day['France'] = 33
+start_day['India'] = 30
+start_day['Indonesia'] = 30
+start_day['Pakistan'] = 30
 
 country_fit = {}
 country_active_cases = {}
@@ -107,8 +127,12 @@ country_I0 = {}
 for country in model_countries:
     country_cases = confirmed.loc[confirmed['Country/Region']==country].sum()
     country_recovered = recovered.loc[recovered['Country/Region']==country].sum()
+    if(len(country_recovered)<len(country_cases)):
+        last_rec_day = len(country_recovered)
+        country_recovered.loc[country_cases.index[-1]] = country_recovered.iloc[last_rec_day-1]*country_recovered.iloc[last_rec_day-1]/country_recovered.iloc[last_rec_day-2]
+    
     country_deaths = deaths.loc[deaths['Country/Region']==country].sum()
-    country_active = 1 + country_cases[5:] - country_recovered[5:] - country_deaths[5:]
+    country_active = pd.Series(1 + country_cases[5:].values - country_recovered[5:].values - country_deaths[5:].values, index=country_cases.index[5:])
     country_active_cases[country] = country_active
     country_R0[country] = (country_recovered[4+start_day[country]] + country_deaths[4+start_day[country]]) / population[country]
     country_I0[country] = country_active[4+start_day[country]] / population[country] 
@@ -125,10 +149,7 @@ for country in model_countries:
     ydata = ydata[start_day[country]:]
     xdata_long = xdata_long[start_day[country]:]
     
-    N = 1.0
-    # I0 = ydata[0]
-    # S0 = N - I0
-    # R0 = 3
+
     
     #define function to optimize using country level S0, I0, R0
     def temp_odeint(x, beta, gamma):
@@ -149,5 +170,5 @@ for country in model_countries:
     plt.plot(xdata, ydata, 'o')
     plt.plot(xdata_long, fitted)
     plt.title(country)
-    plt.yscale('linear')
+    plt.yscale('log')
         
